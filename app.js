3baf26d081a4bc5da9453ece3a1d21e11cabe6d5
Kaleb Kuo <klkuo.cx@gmail.com>
2020-09-10 15:20:09 +0800

    feat: set view engine with handlebars
    - include handlebars and bootstrap
    - build partial templates

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // Include packages
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+// Set view engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // Set routes
 app.get('/', (req, res) => {
-  res.send('Login Handcraft is coming soon!!')
+  res.redirect('login')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
 // Listen to server
